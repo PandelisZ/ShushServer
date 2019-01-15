f4d24b848d08eb2b0d9e5fcd5e6175fbc7836f96
Pandelis Zembashis <info@pandelis.me>
2019-01-15 10:43:43 +0000

    Gcloud notifications

--- routes/messages.js
+++ routes/messages.js
@@ -2,15 +2,58 @@ var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://shush-notifications.firebaseio.com"
+});
+
 router.post('/send', function(req, res) {
   models.Message.create({
     payload: req.body.payload,
     recipient: req.body.recipient,
     sender: req.body.sender
   }).then(function() {
-    res.send({
-      status: 'ok'
-    });
+    models.User.findOne({
+      where: {
+        id: req.body.recipient
+      }
+    })
+  }).then(function(user) {
+    // See documentation on defining a message payload.
+
+    if(user) {
+      var message = {
+        data: {
+          notification: {
+            title: 'New Message',
+            body: 'New Message'
+          }
+        },
+        token: registrationToken
+      };
+
+      // Send a message to the device corresponding to the provided
+      // registration token.
+      admin.messaging().send(message)
+        .then((response) => {
+          res.send({
+            status: 'notified'
+          });
+        })
+        .catch((error) => {
+          res.send({
+            status: 'ok'
+          });
+        });
+    } else {
+      res.send({
+        status: 'ok'
+      });
+    }
   });
 });
 
